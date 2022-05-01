2d36afb5406c4e15835186c88777723ecf4872a0
noahthomlison <noahthomlison@gmail.com>
2022-05-01 16:31:02 -0700

    adds endpoint for home

--- djangoServer/djangoServer/urls.py
+++ djangoServer/djangoServer/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home),
+    # path('search/', include("searchApp.urls"))
 ]
