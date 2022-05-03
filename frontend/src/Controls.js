4068a854fe10dba6b9f10ecc13cdc96c21c2e485
noahthomlison <noahthomlison@gmail.com>
2022-05-03 10:04:41 -0700

    adds eventHandlers to capture user input

--- frontend/src/Controls.js
+++ frontend/src/Controls.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import {Dropdown, InputGroup, FormControl, Button, Form} from 'react-bootstrap'
-function Controls() {
+function Controls({setArray, setSortType}) {
+
+  function handleChangeSlider(event) {
+    let value = event.target.value
+    console.log(value)
+    setArray(value)
+  }
+
+  function handleChangeType(type) {
+    console.log(type)
+    setSortType(type)
+  }
+
   return (
     <div>
       <Dropdown>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
+        <Dropdown.Toggle variant="success" id="dropdown-basic" >
           Sort Algos
         </Dropdown.Toggle>
         <Dropdown.Menu>
-          <Dropdown.Item href="#/action-1">Selection Sort</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Bubble Sort</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Insertion Sort</Dropdown.Item>
-          <Dropdown.Item href="#/action-4">Merge Sort</Dropdown.Item>
-          <Dropdown.Item href="#/action-5">Quick Sort</Dropdown.Item>
-          <Dropdown.Item href="#/action-6">Heap Sort</Dropdown.Item>
+          <Dropdown.Item value="Selection" onClick={() => handleChangeType("Selection")}>Selection Sort</Dropdown.Item>
+          <Dropdown.Item value="Bubble" onClick={() => handleChangeType("Bubble")}>Bubble Sort</Dropdown.Item>
+          <Dropdown.Item value="Insertion" onClick={() => handleChangeType("Insertion")}>Insertion Sort</Dropdown.Item>
+          <Dropdown.Item value="Merge" onClick={() => handleChangeType("Merge")}>Merge Sort</Dropdown.Item>
+          <Dropdown.Item value="Quick" onClick={() => handleChangeType("Quick")}>Quick Sort</Dropdown.Item>
+          <Dropdown.Item value="Heap" onClick={() => handleChangeType("Heap")}>Heap Sort</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>        
       <InputGroup className="mb-3">
         <Form.Label>Size of Array</Form.Label>
-        <Form.Range />
+        <Form.Range onChange={handleChangeSlider}/>
       </InputGroup>
     <h1>Controls</h1>
     </div>
