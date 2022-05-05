7c9f913e4646fd5ce76682ca655114ea45af9e5b
noahthomlison <noahthomlison@gmail.com>
2022-05-04 20:17:12 -0700

    removes commented out filler code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,19 +19,3 @@ function App() {
 }
 
 export default App;
-
-// <header className="App-header">
-// <img src={logo} className="App-logo" alt="logo" />
-// <p>
-//   Edit <code>src/App.js</code> and save to reload.
-// </p>
-// <a
-//   className="App-link"
-//   href="https://reactjs.org"
-//   target="_blank"
-//   rel="noopener noreferrer"
-// >
-//   Learn React
-// </a>
-// <Button type="button" class="btn btn-primary">Primary</Button>
-// </header>
