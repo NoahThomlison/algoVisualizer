c4eee7fcd7b818d1ad52ce78d067127d1823a50e
noahthomlison <noahthomlison@gmail.com>
2022-05-06 22:06:00 -0700

    working to change array to a state variavble to fix error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,27 @@ import Controls from './Controls'
 import { useState } from 'react';
 
 function App() {
-  const [arrayLength, setArrayLength] = useState()
+  const createArray = () => {
+    let temp = []
+    for(let i = 0; i <= arrayLength; i++){
+      let number = (Math.floor(Math.random(0, 1)*100))
+      if(number === 0) {
+        number++
+      }
+      temp.push(number)
+    }
+    return(temp)
+  }
+
+  const [arrayLength, setArrayLength] = useState(100)
   const [sortType, setSortType] = useState()
-  const [start, setStart] = useState()
+  const [start, setStart] = useState(false)
+  const [array, setArray] = useState(createArray())
 
   return (
     <div className="App">
-      <Controls setStart={setStart} setArrayLength={setArrayLength} setSortType={setSortType}></Controls>
-      <Vizualizer start={start} arrayLength={arrayLength} sortType={sortType}></Vizualizer>
+      <Controls setStart={setStart} setArrayLength={setArrayLength} setSortType={setSortType} setArray={setArray}></Controls>
+      {/* <Vizualizer setArray={setArray} array={array} start={start} arrayLength={arrayLength} sortType={sortType}></Vizualizer> */}
     </div>
   );
 }
--- frontend/src/Vizualizer.js
+++ frontend/src/Vizualizer.js
@@ -1,49 +1,31 @@
 import './App.css';
 import { useState } from 'react';
 
-function Vizualizer({start, sortType, arrayLength}) {
-  
-  const createArray = () => {
-    let temp = []
-    for(let i = 0; i <= arrayLength; i++){
-      let number = (Math.floor(Math.random(0, 1)*100))
-      if(number === 0) {
-        number++
-      }
-      temp.push(number)
-    }
-    return(temp)
-  }
-  let array = createArray()
-  const [sortedArray, setSortedArray] = useState([createArray()])
-
-  function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
+function Vizualizer({array, setArray, start, sortType, arrayLength}) {
   const timer = ms => new Promise(res => setTimeout(res, ms))
-
   const changeColor = (targer, color) => {
     document.getElementById(`${targer}`).style.backgroundColor = `${color}`;
-
   }
+  
+  function delayFun(){
+    setTimeout(function()  { console.log("Jacob"); }, 300); 
+ }
+ 
   async function selectionSort (array) {
-    let minIndex 
-    let holder
-    let heightHolder
-    console.log(array.length)
     console.log(array)
     let delay = 1
     for (let i = 0 ; i < array.length; i++){
-      console.log("new row")
-        // changeColor(i, "green")
+      setTimeout(function timer() {
+        let holder
+        let heightHolder
+        changeColor(i, "green")
         // await timer(delay);
         for (let j = (i + 1) ; j < array.length; j++){
-          // changeColor(j, "red")
+          changeColor(j, "red")
           // await timer(delay);
           if(array[i] > array[j]){
-            console.log(array[i])
-            console.log(`${array[i]} > ${array[j]}`)
+            // console.log(array[i])
+            // console.log(`${array[i]} > ${array[j]}`)
             holder = array[i]
             heightHolder = `${array[i]*5}px`
 
@@ -54,24 +36,20 @@ function Vizualizer({start, sortType, arrayLength}) {
             array[j] = holder
             document.getElementById(`${j}`).innerText = holder
             document.getElementById(`${j}`).style.height = heightHolder
-
           }
-          // changeColor(j, "blue")
+          changeColor(j, "blue")
           // await timer(delay);
         }
-        // await timer(delay);
-        // changeColor(i, "blue")
-
+      }, i * 100);
+      console.log(array)
     }
-    console.log(array)
     return(array)
   }
 
   if(start){
-    const timer = setInterval(selectionSort(array), 20000)
-    // timer()
-    // selectionSort(array)
+    selectionSort(array)
   }
+  
 
   return (
     <div className="vizualizerWrapper">
