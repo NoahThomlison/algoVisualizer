23de9214c7900ee41312725659042230b8544080
noahthomlison <noahthomlison@gmail.com>
2022-05-10 19:57:10 -0700

    adds working quickSort without visualiztion

--- frontend/src/Vizualizer.js
+++ frontend/src/Vizualizer.js
@@ -1,15 +1,33 @@
 import './App.css';
 import selectionSort from './helperFunctions/selectionSort';
+import quickSort from './helperFunctions/quickSort'
 
 function Vizualizer({array, setArray, start, sortType, arrayLength}) {
   let blue = 5
   let red = 2
   let green = 3
   const timer = ms => new Promise(res => setTimeout(res, ms))
+  let delay = 10
 
  
   if(start){
-    selectionSort(array, blue, red, green)
+    switch(sortType) {
+      case "Selection":
+        selectionSort(array, blue, red, green, delay)
+        break;
+      case "Bubble":
+        break;
+      case "Insertion":
+        break;
+      case "Merge":
+        break;
+      case "Quick":
+        console.log("quick")
+        quickSort(array, 0, array.length - 1, blue, red, green, delay)        
+        break;
+      default:
+    }
+    
   }
   
   return (
--- frontend/src/helperFunctions/quickSort.js
+++ frontend/src/helperFunctions/quickSort.js
@@ -1,68 +1,42 @@
 import changeColor from './changeColor';
 import timer from './timer'
 
-function partition(array, l, r)
-{
-  let pivot = array[r]
-  let i = l - 1
-  let holder
-  for(let j = l; j <= r - 1; j++){
-    if(array[j] < pivot){
-      i += 1 
-      holder = array[j]
-      array[j] = array[i]
-      array[i] = holder
-    }
-  }
 
-  holder = array[r]
-  array[r] = array[i+1]
-  array[i+1] = holder
-  
-  return(i+1)
+function partition(array, left, right) {
+  let holder
+  var pivot   = array[Math.floor((right + left) / 2)], //middle element
+      i       = left, //left pointer
+      j       = right; //right pointer
+  while (i <= j) {
+      while (array[i] < pivot) {
+          i++;
+      }
+      while (array[j] > pivot) {
+          j--;
+      }
+      if (i <= j) {
+        holder = array[j]
+        array[j] = array[i]
+        array[i] = holder
+          i++;
+          j--;
+      }
+  }
+  return i;
 }
 
-async function quickSort (array, l, r, blue, red, green) {
-  if(l >= r){
-    return
+function quickSort(items, left, right) {
+  var index;
+  if (items.length > 1) {
+      index = partition(items, left, right); //index returned from partition
+      if (left < index - 1) { //more elements on the left side of the pivot
+          quickSort(items, left, index - 1);
+      }
+      if (index < right) { //more elements on the right side of the pivot
+          quickSort(items, index, right);
+      }
   }
-
-  let p = partition(array, l, r)
-  quickSort(array, l, p - 1)
-  quickSort(array, l, p + 1)
-  return(array)
+  return items;
 }
 
 export default quickSort;
-
-
-
-// console.log(array)
-// let delay = 10
-// let minIndex
-// let holder
-// let heightHolder
-// for (let i = 0 ; i < array.length; i++){
-//     minIndex = i
-//     for (let j = (i + 1) ; j < array.length; j++){
-//       changeColor(j, "red")
-//       await timer(delay);
-//       if(array[minIndex] >= array[j]){
-//         minIndex = j
-//       }
-//       changeColor(j, `rgb(${document.getElementById(`${j}`).innerText*red}, ${document.getElementById(`${j}`).innerText*green}, ${document.getElementById(`${j}`).innerText*blue})`)
-//     }
-//     holder = array[i]
-//     heightHolder = `${array[i]*5}px`
-//     await timer(delay);
-//     document.getElementById(`${i}`).innerText = array[minIndex]
-//     array[i] = array[minIndex]
-//     document.getElementById(`${i}`).style.height = `${array[minIndex]*5}px`
-//     await timer(delay);
-//     document.getElementById(`${minIndex}`).innerText = holder
-//     array[minIndex] = holder
-//     document.getElementById(`${minIndex}`).style.height = heightHolder
-//     await timer(delay);
-//     changeColor(i, `rgb(${ document.getElementById(`${i}`).innerText*red}, ${ document.getElementById(`${i}`).innerText*green}, ${ document.getElementById(`${i}`).innerText*blue})`)
-//     await timer(delay);
-//     changeColor(minIndex, `rgb(${document.getElementById(`${minIndex}`).innerText*red}, ${document.getElementById(`${minIndex}`).innerText*green}, ${document.getElementById(`${minIndex}`).innerText*blue})`)
